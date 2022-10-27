a669435affc5f2c83773bc562a755e11bac6d3d1
deyvisonborges <deyvisonsilvaborges@gmail.com>
2022-10-27 14:00:37 -0300

    inital configs

---
+++ src/test/java/com/cliente/rasmoo/plus/AwsRasmooPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliente.rasmoo.plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsRasmooPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
