6109fa8fbdacf7764c309c04aa84c351a383e3b1
deyvisonborges <deyvisonsilvaborges@gmail.com>
2022-11-07 15:49:34 -0300

    add resource handler

---
+++ src/main/java/com/cliente/rasmoo/plus/exceptions/handlers/ResourceHandler.java
@@ -0,0 +1,18 @@
+package com.cliente.rasmoo.plus.exceptions.handlers;
+
+import com.cliente.rasmoo.plus.exceptions.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ResourceHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> notFoundException(NotFoundException n) {
+        String errorMessage = n.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    }
+
+}
