bd15a9afcd99ab4024668bc214868e2a3a22da45
deyvisonborges <deyvisonsilvaborges@gmail.com>
2022-11-07 15:49:49 -0300

    add notfound exception

---
+++ src/main/java/com/cliente/rasmoo/plus/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.cliente.rasmoo.plus.exceptions;
+
+public class NotFoundException extends RuntimeException   {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
