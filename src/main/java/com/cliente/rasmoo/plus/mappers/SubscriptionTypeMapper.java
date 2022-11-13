4e615066d7806be1d51910991723a7a987207af5
deyvisonborges <deyvisonsilvaborges@gmail.com>
2022-11-13 02:37:33 -0300

    add illegal state exception to subscription type mapper

--- src/main/java/com/cliente/rasmoo/plus/mappers/SubscriptionTypeMapper.java
+++ src/main/java/com/cliente/rasmoo/plus/mappers/SubscriptionTypeMapper.java
@@ -4,6 +4,11 @@ import com.cliente.rasmoo.plus.dtos.SubscriptionTypeDto;
 import com.cliente.rasmoo.plus.models.SubscriptionTypeModel;
 
 public class SubscriptionTypeMapper {
+
+    private SubscriptionTypeMapper() {
+        throw new IllegalStateException("Você não pode instanciar essa classe de utilitário");
+    }
+
     public static SubscriptionTypeModel fromDtoToEntity(SubscriptionTypeDto dto) {
         return SubscriptionTypeModel
             .builder()
