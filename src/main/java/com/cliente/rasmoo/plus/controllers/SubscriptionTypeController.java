d9c92bd95cb577ddeaa8801aba8813d63b48e95a
deyvisonborges <deyvisonsilvaborges@gmail.com>
2022-11-13 14:25:10 -0300

    add validations with javax.validation

--- src/main/java/com/cliente/rasmoo/plus/controllers/SubscriptionTypeController.java
+++ src/main/java/com/cliente/rasmoo/plus/controllers/SubscriptionTypeController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -27,7 +28,7 @@ public class SubscriptionTypeController {
     }
 
     @PostMapping
-    public ResponseEntity<SubscriptionTypeModel> create(@RequestBody SubscriptionTypeDto dto) {
+    public ResponseEntity<SubscriptionTypeModel> create(@Valid @RequestBody SubscriptionTypeDto dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(subscriptionTypeService.create(dto));
     }
 
--- src/main/java/com/cliente/rasmoo/plus/dtos/SubscriptionTypeDto.java
+++ src/main/java/com/cliente/rasmoo/plus/dtos/SubscriptionTypeDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Data
@@ -13,8 +17,17 @@ import java.math.BigDecimal;
 @Builder
 public class SubscriptionTypeDto {
     private Long id;
+    @NotBlank(message = "campo name não pode ser nulo ou vazio")
+    @Size(min = 5, max = 30)
     private String name;
+
+    @Max(value = 12, message = "campo access_month não pode ser maior que 12")
     private Long accessMonth;
+
+    @NotNull(message = "campo price não pode ser nulo")
     private BigDecimal price;
+
+    @NotBlank(message = "campo product_key não pode ser nulo ou vazio")
+    @Size(min = 5, max = 15, message = "campo product_key deve ter tamanho entre 5 e 15")
     private String productKey;
 }
