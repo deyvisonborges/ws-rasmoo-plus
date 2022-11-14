4c9aa5724beab85978ba5de592768882efbe4918
deyvisonborges <deyvisonsilvaborges@gmail.com>
2022-11-14 01:38:36 -0300

    mapping errors from javax.validation

--- src/main/java/com/cliente/rasmoo/plus/exceptions/handlers/ResourceHandler.java
+++ src/main/java/com/cliente/rasmoo/plus/exceptions/handlers/ResourceHandler.java
@@ -5,12 +5,17 @@ import com.cliente.rasmoo.plus.exceptions.BadRequestException;
 import com.cliente.rasmoo.plus.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class ResourceHandler {
-
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponseDto> notFoundException(NotFoundException n) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponseDto
@@ -30,6 +35,23 @@ public class ResourceHandler {
             .statusCode(HttpStatus.BAD_REQUEST.value())
             .build()
         );
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> methodArgumentNotValidException(MethodArgumentNotValidException m) {
+        Map<String, String> messages = new HashMap<>();
+        m.getBindingResult().getAllErrors().forEach(err -> {
+            String field = ((FieldError) err).getField();
+            String message = err.getDefaultMessage();
+            messages.put(field, message);
+        });
+
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDto
+            .builder()
+            .message(Arrays.toString(messages.entrySet().toArray()))
+            .httpStatus(HttpStatus.BAD_REQUEST)
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .build());
     }
 
 }
