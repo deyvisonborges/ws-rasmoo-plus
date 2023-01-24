24f293c156bb4dfaaa4e41538004e277b51b3256
Deyvison Borges <deyvison.borges@icloud.com>
2023-01-24 08:40:31 -0300

    create customer and integration with raspay

--- src/main/java/com/cliente/rasmoo/plus/services/PaymentProcessService.java
+++ src/main/java/com/cliente/rasmoo/plus/services/PaymentProcessService.java
@@ -4,7 +4,9 @@ import com.cliente.rasmoo.plus.dtos.PaymentProcessDto;
 import com.cliente.rasmoo.plus.dtos.UserPaymentInfoDto;
 import com.cliente.rasmoo.plus.exceptions.BusinessException;
 import com.cliente.rasmoo.plus.exceptions.NotFoundException;
+import com.cliente.rasmoo.plus.integrations.WsRaspayIntegration;
 import com.cliente.rasmoo.plus.mappers.UserPaymentInfoMapper;
+import com.cliente.rasmoo.plus.mappers.wsraspay.CustomerMapper;
 import com.cliente.rasmoo.plus.repositories.UserPaymentInfoRepository;
 import com.cliente.rasmoo.plus.repositories.UserRepository;
 import com.cliente.rasmoo.plus.services.rules.PaymentProcessRules;
@@ -16,10 +18,16 @@ import java.util.Objects;
 public class PaymentProcessService implements PaymentProcessRules {
     private final UserRepository userRepository;
     private final UserPaymentInfoRepository userPaymentInfoRepository;
+    private final WsRaspayIntegration wsRaspayIntegration;
 
-    PaymentProcessService(UserRepository userRepository, UserPaymentInfoRepository userPaymentInfoRepository) {
+    PaymentProcessService(
+            UserRepository userRepository,
+            UserPaymentInfoRepository userPaymentInfoRepository,
+            WsRaspayIntegration wsRaspayIntegration
+    ) {
         this.userRepository = userRepository;
         this.userPaymentInfoRepository = userPaymentInfoRepository;
+        this.wsRaspayIntegration = wsRaspayIntegration;
     }
     @Override
     public Boolean process(PaymentProcessDto paymentProcessDto) {
@@ -34,6 +42,8 @@ public class PaymentProcessService implements PaymentProcessRules {
         if(Objects.nonNull(user.getSubscriptionTypeModel())) {
             throw new BusinessException("Pagamento não pode ser processado pois usário já possui assinatura.");
         }
+
+        var customerDto = wsRaspayIntegration.createCustomer(CustomerMapper.mapper(user));
 
         // salvar informações de pagamento
         var userPaymentInfo = UserPaymentInfoMapper.mapper(paymentProcessDto.getUserPaymentInfoDto(), user);
