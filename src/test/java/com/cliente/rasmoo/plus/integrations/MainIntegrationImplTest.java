622a917f6febd9d02043ebaaf420deffd148e769
Deyvison Borges <deyvison.borges@icloud.com>
2023-01-25 22:55:42 -0300

    add mail configurations

---
+++ src/test/java/com/cliente/rasmoo/plus/integrations/MainIntegrationImplTest.java
@@ -0,0 +1,19 @@
+package com.cliente.rasmoo.plus.integrations;
+
+import com.cliente.rasmoo.plus.integrations.impl.MailIntegrationImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainIntegrationImplTest {
+    private final MailIntegrationImpl mailIntegration;
+
+    public MainIntegrationImplTest(MailIntegrationImpl mailIntegration) {
+        this.mailIntegration = mailIntegration;
+    }
+
+    @Test
+    void send() {
+       mailIntegration.send("meuemail@gmail.com", "senhadoemail");
+    }
+}
