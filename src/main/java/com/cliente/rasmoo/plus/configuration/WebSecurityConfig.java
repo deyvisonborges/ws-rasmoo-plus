afe7169c5f3953b7ad4cdb2d8a58b0ab9cfcc917
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-01-28 22:24:19 -0300

    enableding endpoints

---
+++ src/main/java/com/cliente/rasmoo/plus/configuration/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.cliente.rasmoo.plus.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/subscription-type")
+                .permitAll()
+            .antMatchers(HttpMethod.GET, "/subiscription-type/*")
+                    .permitAll();
+        return httpSecurity.build();
+    }
+}
